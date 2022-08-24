36ad717ff664df7a96c6f5e4acb76eb5967a2d68
Andy Ngo <andytpngo@g.ucla.edu>
2022-08-24 10:46:40 -0700

    updated favicons and added necessary dependencies for SSR with styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>BruinBites</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
