f7d49443d6082685a82e5fc2bf728031d24e41cc
Andy Ngo <andytpngo@g.ucla.edu>
2022-09-02 10:12:08 -0700

    border testing

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -3,8 +3,8 @@ import { signOut } from "next-auth/react";
 
 const Wrapper = ({title, children}) => {
     return (
-        <div className="p-4 flex flex-col mb-16">
-            <div className="flex justify-between">
+        <div className="p-4 flex flex-col mb-16 border-4 border-purple-400">
+            <div className="flex justify-between border-4 border-orange-400">
                 <p className="text-4xl text-primary-blue font-semibold">{title}</p>
                 <button className="text-lg text-primary-blue" onClick={() => signOut({callbackUrl: `${window.location.origin}`})}><IoMdExit size="2em"/></button>
             </div>
