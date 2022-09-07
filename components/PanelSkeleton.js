3acb4818da6ca52c290ef1e4ddeef8bf52da7430
Andy Ngo <andytpngo@g.ucla.edu>
2022-09-06 20:05:56 -0700

    add carousel light/dark skeleton

--- components/PanelSkeleton.js
+++ components/PanelSkeleton.js
@@ -1,11 +1,13 @@
 import { motion } from 'framer-motion';
-
+import { usePrefersColorScheme } from '@anatoliygatt/use-prefers-color-scheme';
+import React, { useEffect } from 'react';
 
 function PanelSkeleton(props)
 {
+    const preferredColorScheme = usePrefersColorScheme();
     return (
         <motion.div 
-        animate={{ backgroundColor: ['#272727', '#1F1F1F', '#272727']}}
+        animate={ preferredColorScheme === 'dark' ? {backgroundColor: ['#272727', '#1F1F1F', '#272727']} : {backgroundColor: ['#FFF', '#EDEDED','#FFF']}}
         transition={{duration: 1.5, repeat: Infinity, repeatDelay: 0, ease: 'easeInOut'}}
         className='w-fooditem select-none shadow-md h-[150px] rounded-xl shrink-0'>
 
