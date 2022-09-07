47d03c41e22d9303d2b774005d72a4e803563c98
Andy Ngo <andytpngo@g.ucla.edu>
2022-09-07 12:05:32 -0700

    redesign landing page button

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,19 +16,17 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col lg:justify-center items-center h-screen gap-32">
-      <img alt="logo" className="dark:drop-shadow-xl w-2/3 lg:w-96 hover:scale-[1.1] max-w-[300px] lg:max-w-[50vw] duration-300 mt-20 lg:mt-0" src="/logo.svg"/>
+    <div className="flex flex-col justify-around items-center h-screen">
+      <img alt="logo" className="dark:drop-shadow-xl w-2/3 lg:w-96 hover:scale-[1.1] max-w-[300px] lg:max-w-[50vw] duration-300" src="/logo.svg"/>
 
-      <div className="flex flex-col lg:flex-row gap-5">
         <motion.button 
-          className="border-4 border-primary-blue rounded-xl w-[200px] px-6 py-4 text-2xl text-primary-blue flex items-center space-around justify-around" 
+          className="border-[5px] border-secondary rounded-[30px] w-[200px] px-6 py-4 text-2xl text-secondary flex items-center space-around justify-around bg-white" 
           whileHover={{scale: 1.1}}
           onClick={() => signIn("google", {callbackUrl: `${window.location.origin}/profile`})}  
           whileTap={{scale: 0.9}}
         >
-            <FcGoogle/><p>SIGN IN</p>
+            <FcGoogle size={35}/><p>SIGN IN</p>
         </motion.button>
-      </div>
     </div>
   )
   
