bc04c99dc714e2a54fff0d05f615cd07fa7e267b
Andy Ngo <andytpngo@g.ucla.edu>
2022-09-06 21:01:55 -0700

    return seperate skeletons

--- components/PanelSkeleton.js
+++ components/PanelSkeleton.js
@@ -5,13 +5,23 @@ import React, { useEffect } from 'react';
 function PanelSkeleton(props)
 {
     const preferredColorScheme = usePrefersColorScheme();
+    if (preferredColorScheme === 'dark')
+    {
+        return (
+            <motion.div
+                animate={{backgroundColor: ['#272727', '#1F1F1F', '#272727']}}
+                transition={{duration: 1.5, repeat: Infinity, repeatDelay: 0, ease: 'easeInOut'}}
+                className='w-fooditem select-none shadow-md h-[150px] rounded-xl shrink-0 bg-[#272727]'
+            />
+        )
+    }
     return (
         <motion.div 
-        animate={ preferredColorScheme === 'dark' ? {backgroundColor: ['#272727', '#1F1F1F', '#272727']} : {backgroundColor: ['#FFF', '#EDEDED','#FFF']}}
-        transition={{duration: 1.5, repeat: Infinity, repeatDelay: 0, ease: 'easeInOut'}}
-        className='w-fooditem select-none shadow-md h-[150px] rounded-xl shrink-0'>
-
-        </motion.div>
+            animate={{backgroundColor: ['#FFF', '#EDEDED','#FFF']}}
+            transition={{duration: 1.5, repeat: Infinity, repeatDelay: 0, ease: 'easeInOut'}}
+            className='w-fooditem select-none shadow-md h-[150px] rounded-xl shrink-0 bg-[#FFF]'
+        />
+                
     )
 }
 
